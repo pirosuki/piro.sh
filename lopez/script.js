85b7aa0f53e1ad57e10d3afe97f1988e01e7a63d
Piro <36814481+Pirosuki@users.noreply.github.com>
2024-01-29 03:59:18 +0100

    Add files via upload

--- lopez/script.js
+++ lopez/script.js
@@ -1,6 +1,47 @@
-let lopez = document.getElementById('lopez');
-let fish = new Audio('./fish.mp3');
-lopez.addEventListener('click', function() {
-    fish.play();
-    fish.currentTime = 0;
+// Grab popout parameter
+const urlParams = new URLSearchParams(window.location.search);
+const popout = urlParams.get('popout');
+
+// If it isnt either true or false then open a popout window with it set to true
+if (popout != 'true' && popout != 'false') {
+	window.open(window.location.href+'?popout=true', '', 'menubar=0');
+	window.close();
+};
+
+// Get mediaSource
+const mediaSource = document.getElementById('mediaSource');
+
+// Get preferred devices
+let preferredVideoId;
+let preferredAudioId;
+navigator.mediaDevices.enumerateDevices()
+.then(devices => { devices.forEach(device => {
+	// Variables for preferred devices
+		if (device.label.includes('Game Capture')) {
+			if (device.kind === 'videoinput') {
+				preferredVideo = device.deviceId;
+
+				console.log("video: ", device.label);
+			}
+			else if (device.kind === 'audioinput') {
+				preferredAudio = device.deviceId;
+
+				console.log("audio: ", device.label);
+			};
+		};
+	})
+
+	// Get webcam
+	if (navigator.mediaDevices.getUserMedia) {
+		navigator.mediaDevices.getUserMedia({ video: { deviceId: preferredVideoId}, audio: { deviceId: preferredAudioId, echoCancellation: false}})
+		.then(stream => {
+			console.log(stream)
+			mediaSource.srcObject = stream;
+		})
+		.catch(err => {
+			console.log("Brokie!");
+			console.log(err);
+		});
+	};
 });
+
