c2d870fadbd3a5312a536631119d599ea9ddb1ce
Piro <36814481+Pirosuki@users.noreply.github.com>
2025-02-25 19:15:38 +0100

    Add files via upload

--- lars/script.js
+++ lars/script.js
@@ -1,75 +1,154 @@
-// Grab popout parameter
-const urlParams = new URLSearchParams(window.location.search);
-const popout = urlParams.get('popout');
-
-// If it isnt either true or false then open a popout window with it set to true
+// popout //
+const popout = new URLSearchParams(window.location.search).get('popout');
 if (popout === 'true') {
 	window.open(window.location.href.split('?')[0], '', 'menubar=0');
 	window.close();
 };
+// //
 
-// Get mediaSource
-const mediaDisplay = document.getElementById('mediaDisplay');
-
-// Get preferred devices
-navigator.mediaDevices.enumerateDevices()
-.then(devices => { 
-	// Variables for preferred devices
-	let preferredVideoId;
-	let preferredAudioId;
-
-	devices.forEach(device => {
-		if (device.label.includes('Game Capture')) {
-			if (device.kind === 'videoinput') {
-				preferredVideoId = device.deviceId;
-
-				console.log("video: ", device.label);
-			}
-			else if (device.kind === 'audioinput') {
-				preferredAudioId = device.deviceId;
-
-				console.log("audio: ", device.label);
-			};
-		}
-	})
-	
-	getMediaStream(preferredVideoId, preferredAudioId)
-})
-
-// Get webcam
-function getMediaStream(preferredVideoId, preferredAudioId) {
-	if (navigator.mediaDevices.getUserMedia) {
-		navigator.mediaDevices.getUserMedia({ video: { deviceId: preferredVideoId}, audio: { deviceId: preferredAudioId, echoCancellation: false}})
-		.then(stream => {
-			console.log(stream)
-			mediaDisplay.srcObject = stream;
-		})
-		.catch(err => {
-			console.log("Brokie!");
-			console.log(err);
-		});
-	};
-}
-
-// volumeSlider logic
-const volumeSlider = document.getElementById('volumeSlider');
-volumeSlider.addEventListener('change', function(e) {
-	mediaDisplay.volume = e.currentTarget.value / 100;
-});
-
-// Hide interface after no movement
-
+// hide interface //
 let interfaceTimeout;
-const interfaceItem = document.getElementsByClassName('interfaceItem')[0];
-mediaDisplay.addEventListener('mousemove', function() {
-	interfaceItem.style.opacity = 1;
-	document.body.style.cursor = 'initial';
+document.body.addEventListener('mousemove', function () {
+	for (element of document.getElementsByClassName('interfaceItem')) {
+		element.style.opacity = 1;
+		document.body.style.cursor = 'initial';
+	}
 
 	clearTimeout(interfaceTimeout);
 
 	interfaceTimeout = setTimeout(() => {
-		interfaceItem.style.opacity = 0;
-		document.body.style.cursor = 'none';
-	}, 1000);
+		for (element of document.getElementsByClassName('interfaceItem')) {
+			element.style.opacity = 0;
+			document.body.style.cursor = 'none';
+		}
+	}, 3000);
+
+});
+// //
+
+// volume //
+const volumeSlider = document.getElementById('volumeSlider');
+volumeSlider.addEventListener('change', function () {
+	mediaDisplay.volume = localStorage.volume = volumeSlider.value / 100;
 });
 
+volumeSlider.value = localStorage.volume * 100;
+// //
+
+// options //
+let optionsElement = document.getElementById('options');
+let optionsButtonElement = document.getElementById('optionsButton');
+optionsButtonElement.addEventListener('click', function () {
+	if (window.getComputedStyle(optionsElement).display === 'none') {
+		optionsElement.style.display = 'block';
+	}
+	else {
+		optionsElement.style.display = 'none'
+	};
+});
+
+let qualitySelectElement = document.getElementById('qualitySelect');
+qualitySelectElement.addEventListener('change', function () {
+	let [width, height, frameRate] = qualitySelectElement.value.split('x');
+	localStorage.width = width;
+	localStorage.height = height;
+	localStorage.frameRate = frameRate;
+
+	mediaDisplay.srcObject.getVideoTracks()[0].applyConstraints({
+		width: { exact: width },
+		height: { exact: height },
+		frameRate: { exact: frameRate }
+	});
+});
+
+function getOptions(track) {
+	const commonishResolutions = [
+		{ width: 7680, height: 4320 },
+		{ width: 3840, height: 2160 },
+		{ width: 2560, height: 1440 },
+		{ width: 1920, height: 1080 },
+		{ width: 1400, height: 1050 },
+		{ width: 1280, height: 1024 },
+		{ width: 1280, height: 720 },
+		{ width: 1152, height: 864 },
+		{ width: 1024, height: 768 },
+		{ width: 800, height: 600 },
+		{ width: 640, height: 480 },
+		{ width: 512, height: 448 },
+		{ width: 320, height: 240 },
+		{ width: 320, height: 224 },
+		{ width: 256, height: 240 },
+		{ width: 256, height: 224 }
+	];
+
+	const commonishFrameRates = [
+		540,
+		480,
+		360,
+		240,
+		165,
+		144,
+		120,
+		60,
+		48,
+		30,
+		25,
+		24
+	];
+
+	commonishResolutions.reverse().forEach(function (resolution) {
+		commonishFrameRates.reverse().forEach(function (frameRate) {
+			track.applyConstraints({
+				width: { exact: resolution.width },
+				height: { exact: resolution.height },
+				frameRate: { exact: frameRate },
+			})
+				.then(() => {
+					const option = document.createElement('option');
+					option.value = option.text = `${resolution.width}x${resolution.height}x${frameRate}`;
+
+					qualitySelectElement.appendChild(option);
+				})
+				.catch(() => { })
+				.finally(() => {
+					track.applyConstraints({
+						width: { ideal: localStorage.width },
+						height: { ideal: localStorage.height },
+						frameRate: { ideal: localStorage.frameRate },
+					});
+				});
+		});
+	});
+}
+// //
+
+// media //
+const mediaDisplay = document.getElementById('mediaDisplay');
+if (navigator.mediaDevices.getUserMedia) {
+	navigator.mediaDevices.getUserMedia({
+		video: true,
+		audio: true
+	})
+		.then(userMedia => {
+			localStorage.videoDeviceId = userMedia.getVideoTracks()[0].getSettings().deviceId;
+			localStorage.audioDeviceId = userMedia.getAudioTracks()[0].getSettings().deviceId;
+
+			navigator.mediaDevices.getUserMedia({
+				video: {
+					deviceId: localStorage.videoDeviceId
+				},
+				audio: {
+					deviceId: localStorage.audioDeviceId,
+					echoCancellation: false
+				}
+			})
+				.then(stream => {
+					mediaDisplay.srcObject = stream;
+					getOptions(stream.getVideoTracks()[0])
+				})
+				.catch(err => {
+					console.log(err);
+				});
+		});
+};
+// //
